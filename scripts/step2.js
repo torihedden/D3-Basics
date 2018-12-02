dcb750375a446ad004effd3ac6d3492e960b2ed2
Tori Hedden <tori.hedden@gmail.com>
2018-12-02 16:39:58 -0500

    add second example with external data

--- scripts/step2.js
+++ scripts/step2.js
@@ -1,20 +1,20 @@
 d3.select('#bar-chart')
 	.append('svg')
-	.attr('width', width)
-	.attr('height', height)
+	.attr('width', config.width)
+	.attr('height', config.height)
 	.style('border', 'solid 1px gray') // equivalent shorthand below in vanilla JS
 	.selectAll('rect').data(data)
 	.enter().append('rect')
 	.style('fill', 'darkgray')
-	.attr('width', barWidth)
+	.attr('width', config.barWidth)
 	.attr('height', function(data) {
-		return data * scaleFactor;
+		return data * config.scaleFactor;
 	})
 	.attr('x', function(data, i) {
-		return i * (barWidth + barOffset)
+		return i * (config.barWidth + config.barOffset)
 	})
 	.attr('y', function(data) {
-		return height - (scaleFactor * data);
+		return config.height - (config.scaleFactor * data);
 	})
 	.on('mouseover', function() {
 		d3.select(this)
--- scripts/step3.js
+++ scripts/step3.js
@@ -1,8 +1,41 @@
+var gameData = {
+	config: {
+		height: 100,
+		width: 800,
+		barWidth: 20,
+		barOffset: 5
+	},
+	title: 'Hours Played on Overwatch Heroes'
+}
+
 d3.csv('data/overwatch-playtime.csv', function(d) {
 	return {
 		hero: d.Hero,
 		timePlayed: +d['Time Played']
 	};
-}).then(function(data) {
-	console.log(data)
-})
+}).then(function(overwatchData) {
+	svg = d3.select('#overwatch-chart')
+		.append('svg')
+		.attr('width', gameData.config.width)
+		.attr('height', gameData.config.height)
+
+	svg.selectAll('rect').data(overwatchData)
+		.enter().append('rect')
+		.style('fill', 'red')
+		.attr('width', gameData.config.barWidth)
+		.attr('height', function(overwatchData) {
+			return overwatchData.timePlayed;
+		})
+		.attr('x', function(overwatchData, i) {
+			return i * (gameData.config.barWidth + gameData.config.barOffset);
+		})
+		.attr('y', function(overwatchData) {
+			return gameData.config.height - overwatchData.timePlayed;
+		})
+
+	svg.append('text')
+		.attr('x', 250)
+		.attr('y', 20)
+		.text(gameData.title)
+
+});
